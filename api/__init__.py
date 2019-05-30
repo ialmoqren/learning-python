2916e14e8244df8fc50275b710dc76785abd2267
Ibrahim Almuqrin <ialmoqren@gmail.com>
2019-05-30 11:23:30 +0300

    Started the /register endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request, json
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.args.get("name")
+    email = request.args.get("email")
+    phone_number = request.args.get("phone_number")
+
+    print(name, email, phone_number)
+
+    response = app.response_class(
+        response=json.dumps({
+            "name": name,
+            "email": email,
+            "phone_number": phone_number
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
