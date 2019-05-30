0de2e168ac529a1beda4626f2918326f80e3d456
Ibrahim Almuqrin <ialmoqren@gmail.com>
2019-05-30 11:55:02 +0300

    handling empty data

--- api/__init__.py
+++ api/__init__.py
@@ -11,19 +11,29 @@ def hello():
 def register():
 
     data = request.get_json()
-    name = data["name"]
-    email = data["email"]
-    phone_number = data["phone_number"]
+    name = data.get("name")
+    email = data.get("email")
+    phone_number = data.get("phone_number")
 
     print(name, email, phone_number)
 
-    response = app.response_class(
-        response=json.dumps({
-            "name": name,
-            "email": email,
-            "phone_number": phone_number
-        }),
-        status=200,
-        mimetype='application/json'
-    )
+    if not name or not email or not phone_number:
+        response = app.response_class(
+            response=json.dumps({
+                "message": "Empty data received",
+                "user_message": "Please fill all the info"
+            }),
+            status=400,
+            mimetype='application/json'
+        )
+    else:
+        response = app.response_class(
+            response=json.dumps({
+                "name": name,
+                "email": email,
+                "phone_number": phone_number
+            }),
+            status=200,
+            mimetype='application/json'
+        )
     return response
