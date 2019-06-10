c501c4132f4c51205f156fca4b51ccfbc4187952
Ibrahim Almuqrin <ialmoqren@gmail.com>
2019-06-10 07:17:24 +0300

    login using hashed passwords

--- api/controller/login.py
+++ api/controller/login.py
@@ -20,7 +20,7 @@ def login():
     else:
         user = Users.query.filter_by(username=username).first()
 
-        if user is None or user.password != password:
+        if user is None or not bcrypt.check_password_hash(user.password, password):
             response = jsonify({
                 "message": "Email or password is not correct",
                 "user_message": "Email or password is not correct"
